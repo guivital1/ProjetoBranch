b6cdfade754904f812d7598684e00206780f8de4
Gui Vital <guivitalrodrigues@gmail.com>
2024-10-03 11:32:55 -0300

    Criando array, objetos e metodos

--- script.js
+++ script.js
@@ -26,9 +26,30 @@ while (w<5);
 function Ola(nome){
     //``(sring literal ou template string ${} chama a variavek)
     console.log(`Seja bem vindo a ${nome}`)
-    alert(`Seja bem vindo a ${nome}`)
+    //alert(`Seja bem vindo a ${nome}`)
     document.write(`Seja bem vindo a ${nome}`)
 }
 
 //executando a função Ola passando o valor
 Ola("fiap")
+
+// Arrow Function
+
+const hello=()=>(console.log("Função arrow function"))
+
+hello()
+
+//ARRAY[], OBJETO {}, METODOS ()
+
+let jogadores = [
+    {nome:"Hugo", idade:17},
+    {nome:"Zezinho", idade:18},
+    {nome:"Luizinho", idade: 19}
+]
+
+let listarJogadores = jogadores.map(function(item){
+    return item.nome
+   
+})
+
+console.log(listarJogadores)
