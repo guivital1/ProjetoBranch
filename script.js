6ae6b7c29c52ce796e3b5ee29f6f7d6ca115f536
Gui Vital <guivitalrodrigues@gmail.com>
2024-10-03 11:24:10 -0300

    Criando função em javascript

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // Estrutura de repetição
 
 for(let i=0; i<10; i++){
-    console.log(i);
+    //console.log(i);
 }
 
 //while
 
 let f = 0;
 while (f<10) {
-    console.log(f)
+    //console.log(f)
     f++
     
 }
+
+let w = 0;
+// do while
+do{
+    //console.log(w);
+    w++;
+}
+while (w<5);
+
+//funções
+
+function Ola(nome){
+    //``(sring literal ou template string ${} chama a variavek)
+    console.log(`Seja bem vindo a ${nome}`)
+    alert(`Seja bem vindo a ${nome}`)
+    document.write(`Seja bem vindo a ${nome}`)
+}
+
+//executando a função Ola passando o valor
+Ola("fiap")
